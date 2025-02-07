abb667388097ed1a98aeffd30d6f1bb070a8fbaa
WelIDev <wellington.andrade.wsa@gmail.com>
2025-02-07 20:51:30 -0300

    Seeding database

---
+++ modeloDominioORM/src/main/java/com/weildev/modelodominioorm/modeloDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.weildev.modelodominioorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class modeloDominioOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(modeloDominioOrmApplication.class, args);
+    }
+
+}
