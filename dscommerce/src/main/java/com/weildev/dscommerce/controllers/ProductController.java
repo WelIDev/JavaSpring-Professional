9d6fd1dc45032d8f0539552b74577da904329edc
WelIDev <wellington.andrade.wsa@gmail.com>
2025-02-08 14:33:55 -0300

    Get product paged

--- dscommerce/src/main/java/com/weildev/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/weildev/dscommerce/controllers/ProductController.java
@@ -3,7 +3,14 @@ package com.weildev.dscommerce.controllers;
 import com.weildev.dscommerce.dto.ProductDTO;
 import com.weildev.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -15,5 +22,10 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return productService.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 }
--- dscommerce/src/main/java/com/weildev/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/weildev/dscommerce/services/ProductService.java
@@ -4,8 +4,12 @@ import com.weildev.dscommerce.dto.ProductDTO;
 import com.weildev.dscommerce.entities.Product;
 import com.weildev.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -17,5 +21,11 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product result = productRepository.findById(id).get();
         return new ProductDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(ProductDTO::new);
     }
 }
