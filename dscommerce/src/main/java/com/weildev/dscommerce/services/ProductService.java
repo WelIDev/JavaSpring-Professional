e284231312a7f11ec643d5fe7c0efecddcc17d68
WelIDev <wellington.andrade.wsa@gmail.com>
2025-02-08 15:40:40 -0300

    Insert product

--- dscommerce/src/main/java/com/weildev/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/weildev/dscommerce/services/ProductService.java
@@ -3,6 +3,7 @@ package com.weildev.dscommerce.services;
 import com.weildev.dscommerce.dto.ProductDTO;
 import com.weildev.dscommerce.entities.Product;
 import com.weildev.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,5 +28,14 @@ public class ProductService {
     public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> result = productRepository.findAll(pageable);
         return result.map(ProductDTO::new);
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO productDTO) {
+        Product entity = new Product();
+        BeanUtils.copyProperties(productDTO, entity);
+
+        entity = productRepository.save(entity);
+        return new ProductDTO(entity);
     }
 }
