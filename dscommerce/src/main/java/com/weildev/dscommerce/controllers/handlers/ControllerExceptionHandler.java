6c07beafc5a16a1755e795ce1c6322b787c053a0
WelIDev <wellington.andrade.wsa@gmail.com>
2025-02-09 15:02:01 -0300

    Exception handling

--- dscommerce/src/main/java/com/weildev/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ dscommerce/src/main/java/com/weildev/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.weildev.dscommerce.controllers.handlers;
 
 import com.weildev.dscommerce.dto.CustomError;
+import com.weildev.dscommerce.services.exceptions.DatabaseException;
 import com.weildev.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,13 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException ex, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- dscommerce/src/main/java/com/weildev/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/weildev/dscommerce/services/ProductService.java
@@ -3,12 +3,16 @@ package com.weildev.dscommerce.services;
 import com.weildev.dscommerce.dto.ProductDTO;
 import com.weildev.dscommerce.entities.Product;
 import com.weildev.dscommerce.repositories.ProductRepository;
+import com.weildev.dscommerce.services.exceptions.DatabaseException;
 import com.weildev.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -41,15 +45,26 @@ public class ProductService {
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO productDTO) {
-        Product entity = productRepository.getReferenceById(id);
-        BeanUtils.copyProperties(productDTO, entity, "id");
+        try{
+            Product entity = productRepository.getReferenceById(id);
+            BeanUtils.copyProperties(productDTO, entity, "id");
 
-        entity = productRepository.save(entity);
-        return new ProductDTO(entity);
+            entity = productRepository.save(entity);
+            return new ProductDTO(entity);
+        }catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
-        productRepository.deleteById(id);
+        if (!productRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
+        try {
+            productRepository.deleteById(id);
+        }catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Referential integrity failure");
+        }
     }
 }
